d83f7850d4920a50acc567047c92918653b61ef4
srscully <srscully@umich.edu>
2022-09-28 19:19:00 -0400

    fixed count_a

--- discussion_5.py
+++ discussion_5.py
@@ -1,59 +1,47 @@
 import unittest
 
-# Counts the number of a's in a sentence (e.g., a string)
+
 def count_a(sentence):
-	total = 0
-	for i in range(len(sentence) - 1):
-		if i == 'a':
-			total += 1
-	return total
+	count = 0
+	for letter in sentence:
+		if letter == 'a':
+			count += 1
+	return count
 
 
-# Item class
-# Describes an item to be sold. Each item has a name, a price, and a stock.
+
+
 class Item:
-	# Constructor.
+	
 	def __init__(self, name, price, stock):
 		self.name = name
 		self.price = price
 		self.stock = stock
 
-	# Print
 	def __str__(self):
 		return ("Item = {}, Price = {}, Stock = {}".format(self.name, self.price, self.stock))
 
-# Warehouse class
-# A warehouse stores items and manages them accordingly.
 class Warehouse:
 
-	# Constructor
 	def __init__(self, items = []):
 		self.items = items[:]
 
-	# Prints all the items in the warehouse, one on each line.	
 	def print_items(self):
 		for item in self.items:
 			print(item)
 			print("\n")	
 
-	# Adds an item to the warehouse	
 	def add_item(self, item):
-		pass
-
-	# Returns the item in the warehouse with the most stock		
-	def get_max_stock(self):
-		pass
+		self.items.append(item)
 	
-	# Returns the item in the warehouse with the highest price
+	def get_max_stock(self):
+		return max(self.items, key = lambda item: item.stock)
+	
 	def get_max_price(self):
-		pass	
+		return max(self.items, key=lambda item: item.price)
 
-
-
-# Tests
 class TestAllMethods(unittest.TestCase):
 
-	# SetUp -- we create a bunch of items for you to use in your tests.
 	def setUp(self):
 		self.item1 = Item("Beer", 6, 20)
 		self.item2 = Item("Cider", 5, 25)
@@ -61,26 +49,22 @@ class TestAllMethods(unittest.TestCase):
 		self.item4 = Item("Fanta", 2, 60)
 		self.item5 = Item("CocaCola", 3, 40)
 
-	## Check to see whether count_a works
 	def test_count_a(self):
-		pass
+		self.assertEqual(count_a(self.item1.name), 0, "count_a failed on item1")
+		self.assertEqual(count_a(self.item2.name), 0, "count_a failed on item2")
+		self.assertEqual(count_a(self.item3.name), 1, "count_a failed on item3")
+		self.assertEqual(count_a(self.item4.name), 2, "count_a failed on item4")
+		self.assertEqual(count_a(self.item5.name), 2, "count_a failed on item5")
 
-
-	## Check to see whether you can add an item to the warehouse
-	def test_add_item(self):
-		pass
-
-
-	## Check to see whether warehouse correctly returns the item with the most stock
-	def test_warehouse_max_stocks(self):
-		pass
-
-
-	# Check to see whether the warehouse correctly return the item with the highest price
-	def test_warehouse_max_price(self):
+	def test_warehouse_add_item(self):
 		pass
 		
-
+	def test_warehouse_get_max_stock(self):
+		pass
+	
+	def test_warehouse_get_max_price(self):
+		pass
+	
 def main():
 	unittest.main()
 
